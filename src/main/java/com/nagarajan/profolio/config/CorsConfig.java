4ebc89849c3c1c6bb37eae6e22f7def6bbad2625
Nagarajan <nnnagarajan16@gmail.com>
2026-07-02 00:16:04 +0530

    added front-end URL

--- src/main/java/com/nagarajan/profolio/config/CorsConfig.java
+++ src/main/java/com/nagarajan/profolio/config/CorsConfig.java
@@ -13,6 +13,7 @@ public class CorsConfig {
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("https://portfolio-frontend-navy-phi.vercel.app");
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
 
